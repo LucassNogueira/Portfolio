b309e717a82588729e02242737715f817c7b75b8
Lucas Nogueira <97054688+LucassNogueira@users.noreply.github.com>
2025-10-17 15:15:20 -0500

    fix build issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const runtime = 'edge'
+
 import { useParams } from 'next/navigation'
 import { Box, Container, Typography, CircularProgress } from '@mui/material'
 import { BlogPostContent } from '@/components/BlogPostContent'
@@ -28,7 +30,7 @@ export default function BlogPostPage() {
           Post Not Found
         </Typography>
         <Typography variant="body1" color="text.secondary">
-          The blog post you're looking for doesn't exist.
+          The blog post you&apos;re looking for doesn&apos;t exist.
         </Typography>
       </Container>
     )
